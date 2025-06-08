76dcfcbec3fa4967f58bb94f94cea2111fc013a1
a.savanovich <asavanster@gmail.com>
2025-06-08 14:35:10 +0200

    syzygymsu solution

--- ya_algo_2020_09/task6_2/main.cpp
+++ ya_algo_2020_09/task6_2/main.cpp
@@ -1,11 +1,17 @@
 // problems/F/
+// syzygymsu solution https://github.com/syzygymsu/quizcpp/blob/master/ya_algo_2020_09/task6_2/main.cpp
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
+#include <utility>
 
-using namespace std;
+#include <assert.h>
+
+
+// using namespace std;
 
 void print(const std::vector<int>& row) {
     std::cout << "[";
@@ -126,6 +132,7 @@ int func_good(std::istream& f, std::ostream& out) {
 
 
 int func(std::istream& f, std::ostream& out) {
+    // ==== Loading ====
     int n, m, k;
     f >> n;
     f >> m;
@@ -134,143 +141,66 @@ int func(std::istream& f, std::ostream& out) {
         out << '0' << std::endl;
         return 0;
     }
-    std::vector<std::vector<int>> rows(m);
-    std::vector<std::vector<int>> cols(m);
-
-    std::vector<int> colsx(n + 1, 0);
-
+    // std::cout << n << " " << m << " " << k << std::endl;
+    std::vector< std::vector<int> > cols(n);
     for (int i = 0; i < k; ++i) {
         int x, y;
         f >> x;
         f >> y;
-        int temp = n - 1 - x;
-        rows[y].push_back(temp);
-        cols[temp].push_back(y);
-        ++colsx[temp];
+        cols.at(x).push_back(y);
     }
-    for (auto& row : rows) {
-        std::sort(row.begin(), row.end(), std::greater<int>());
-    }
-
     for (auto& col : cols) {
-        std::sort(col.begin(), col.end(), std::greater<int>());
+        std::sort(col.begin(), col.end());
     }
-    // print(cols_q);
-    bool color = !rows[0].empty() && rows[0].back() == 0;
-    int full_rows = 0;
-    std::vector<int> positions(m + 1, 0);
-    positions[0] = n;
-    int iteration = 0;
-    int iterCount = 0;
-    int fisrtNonZeroCol = 0;
-    while (full_rows < m) {
-        // std::cout << "while " << full_rows <<" " << iteration << std::endl;
-        for (int i = fisrtNonZeroCol; i < n; ++i) {
-            if (colsx[i] > 0) {
-                fisrtNonZeroCol = i;
-                break;
-                // ++iterCount;
-            }
-        }
-        int localNonZero = colsx[fisrtNonZeroCol];
-        for (int j = full_rows; j < m; ++j) {
-            // std::cout << "for " << j << std::endl;
 
-            auto& row = rows[j];
-            if (color) {
-                // std::cout << "color " << j << std::endl;
+    // ==== Init ====
+    using Segment = std::pair<int, int>;
+    std::vector<Segment> segments(k, Segment{0,0});
+    std::unordered_set<Segment*> unclosed; // segments to be closed
+    auto find_segment = [&](int target) {
+        Segment seg{target + 1, target + 1};
+        auto pp = std::lower_bound(segments.begin(), segments.end(), seg);
+        assert(pp != segments.begin());
+        --pp;
+        return pp;
+    };
 
-                if (row.empty()) {
-                    break;
-                }
-                int x = row.back();
-                int pos = positions[j + 1];
-                if (x == pos) {
-                    while (pos == x && x < positions[j]) {
-                        positions[j + 1] = x + 1;
-                        pos = x + 1;
-                        --colsx[x];
-                        if (x == fisrtNonZeroCol) {
-                            --localNonZero;
-                        }
-                        // cout << "in color " << iterCount << endl;
-                        row.pop_back();
-                        cols[x].pop_back();
-                        ++iterCount;
-                        if (row.empty()) {
-                            break;
-                        }
-                        x = row.back();
-                    }
-                    ++iterCount;
-                    if (localNonZero == 0) {
-                        break;
-                    }
-                }
-                else {
-                    //auto& yvec = cols[positions[j + 1]];
-                    //std::cout << "in color else " << j << " " << yvec.back() << endl;
+    // ==== Process ====
+    for (auto& col :cols) {
+        std::unordered_set<Segment*> unclosed_next;
+        for (int point_idx = col.size() - 1; point_idx >= 0; --point_idx) {
+            int top_y = col[point_idx];
+            auto s = find_segment(top_y);
 
-                    //if (yvec.empty()) {
-                    //    std::cout << "strange" << endl;
-                    //    break;
-                    //}
-                    //j = yvec.back();
-                }
-                // cout << "skip color " << iterCount << endl;
+            // merge consequent points
+            int bottom_y = top_y;
+            while (point_idx > 0 
+                    && col[point_idx-1] >= s->first
+                    && col[point_idx-1] == bottom_y-1) {
+                --point_idx;
+                bottom_y = col[point_idx];
             }
-            else {
-                // std::cout << "non color " << j << std::endl;
 
-                ++iterCount;
-                if (row.empty() || row.back() > positions[j]) {
-                    positions[j + 1] = positions[j];
-                    if (positions[j + 1] >= n) {
-                        // std::cout << "full " << j << std::endl;
-                        full_rows = j + 1;
-                    }
-                    else {
-                        break;
-                    }
-                    // break;
-                }
-                else {
-                    if (row.back() > fisrtNonZeroCol) {
-                        // std::cout << "less1 " << iterCount <<" " << localNonZero << std::endl;
-                        positions[j + 1] = row.back();
-                    }
-                    else if (row.back() == fisrtNonZeroCol) {
-                        // std::cout << "less2 " << iterCount  << " " << localNonZero <<std::endl;
-
-                        --localNonZero;
-                        if (localNonZero == 0) {
-                            positions[j + 1] = fisrtNonZeroCol;
-                            break;
-                        }
-                        positions[j + 1] = fisrtNonZeroCol;
-                    }
-                }
-            }
-            // std::cout << "pre full " << positions[j + 1]  << " "<< j << std::endl;
-            // print(positions);
-            if (positions[j + 1] >= n) {
-                // std::cout << "full " << j << std::endl;
-                full_rows = j + 1;
-            }
-            else if (positions[j + 1] == fisrtNonZeroCol) {
-                break;
-            }
+            s->first = bottom_y;
+            s->second = top_y + 1;
+            unclosed.erase(&*s);
+            unclosed_next.insert(&*s);
         }
 
-        color = !color;
-        ++iteration;
+        // close unclosed segments
+        for(Segment* s : unclosed) {
+            s->first = s->second;
+        }
+        unclosed = std::move(unclosed_next);
     }
-    if (color) {
-        --iteration;
+
+    int res = 0;
+    for (Segment& s : segments) {
+        if (s.first) ++res;
+        if (s.second) ++res;
     }
-    // std::cout << "iterCount " << iterCount << " iteration " << iteration << std::endl;
-    // out << iteration << std::endl;
-    return iteration;
+    out << res << std::endl;
+    return res;
 }
 
 
@@ -284,7 +214,7 @@ void test(const std::string& filename, int expected) {
     int res = func(f, std::cout);
     auto finish = std::chrono::high_resolution_clock::now();
     if (res != expected) {
-        std::cout << "!!!!ERROR in test " + filename << " " << res << std::endl;
+        std::cout << "!!!!ERROR in test " + filename << " " << res << " expected " << expected << std::endl;
     }
     else {
         std::cout << filename << " OK " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << std::endl;
@@ -300,14 +230,13 @@ int main() {
     test("input3.txt", 3);
     test("input30x30.txt", 57);
     test("input30x30p.txt", 57);
-    test("input30x30p2.txt", 57);
-    test("input30.txt", 58);
+    test("input30.txt", 56);
     test("input10.txt", 19);
     test("inputdotslongdots.txt", 11);
     test("inputdots.txt", 10);
     test("inputdots2.txt", 10);
-    // test("input10kx10k.txt", 19997);
-    // test("input100kx100k.txt", 199997);
-    // test("input250kx250k.txt", 499997);
+    test("input10kx10k.txt", 19997);
+    test("input100kx100k.txt", 199997);
+    test("input250kx250k.txt", 499997);
     return 0;
 }
