c8a2e1455f99fa29d376e6ec5ff3b7aad392383f
asavan <asavan@mail.ru>
2025-06-29 19:26:22 +0300

    formatter

--- ya_algo_2020_09/E2/main.cpp
+++ ya_algo_2020_09/E2/main.cpp
@@ -15,27 +15,29 @@ struct Stats {
     int on;
 };
 
-Stats solve_rec(size_t p, const std::vector<Node> &parents, int k);
+Stats solve_rec(size_t p, const std::vector<Node>& parents, int k);
 
-std::vector<Stats> makeStats(size_t p, const std::vector<Node> &parents, int k) {
-    const Node &node = parents[p];
+bool statsSorter(const Stats& l, const Stats& r) { return (l.manual - l.on) < (r.manual - r.on); }
+
+std::vector<Stats> makeStats(size_t p, const std::vector<Node>& nodes, int k) {
+    const Node& node = nodes[p];
     std::vector<Stats> sts;
     sts.reserve(node.child.size());
     for (int ch : node.child) {
-        sts.push_back(solve_rec(ch, parents, k));
+        sts.push_back(solve_rec(ch, nodes, k));
     }
     return sts;
 }
 
-int calcManual(const std::vector<Stats> &sts) {
+int calcManual(const std::vector<Stats>& sts) {
     int manual = 1;
-    for (const auto &s : sts) {
+    for (const auto& s : sts) {
         manual += s.manual;
     }
     return manual;
 }
 
-int calcOn(const std::vector<Stats> &sts, size_t criticalIndex) {
+int calcOn(const std::vector<Stats>& sts, size_t criticalIndex) {
     int on = 0;
     for (size_t i = 0; i < sts.size(); ++i) {
         if (i < criticalIndex) {
@@ -47,12 +49,12 @@ int calcOn(const std::vector<Stats> &sts, size_t criticalIndex) {
     return on;
 }
 
-int calcCascade(const std::vector<Stats> &sts, size_t criticalIndex, int on) {
+int calcCascade(const std::vector<Stats>& sts, size_t criticalIndex, int on) {
     size_t prevCriticalIndex = criticalIndex - 1;
     const Stats& subsCand = sts[prevCriticalIndex];
     int cascade = 0;
     for (size_t i = 0; i < sts.size(); ++i) {
-        const auto &s = sts[i];
+        const auto& s = sts[i];
         int candidatescore = 1 + on + s.cascade;
         if (i < criticalIndex) {
             candidatescore -= s.on;
@@ -68,34 +70,25 @@ int calcCascade(const std::vector<Stats> &sts, size_t criticalIndex, int on) {
     return cascade;
 }
 
-Stats solve_rec(size_t p, const std::vector<Node> &parents, int k) {
+Stats solve_rec(size_t p, const std::vector<Node>& parents, int k) {
     std::vector<Stats> sts = makeStats(p, parents, k);
     int manual = calcManual(sts);
     if (sts.size() < k) {
         return Stats{.manual = manual, .cascade = manual, .on = manual - 1};
     }
 
-    std::sort(sts.begin(), sts.end(), [](const auto &l, const auto &r) {
-        return (l.manual - l.on) < (r.manual - r.on);
-    });
-    /*
-        std::nth_element(sts.begin(), sts.end() - k + 1, sts.end(),
-                         [](const auto &l, const auto &r) {
-                             return (l.manual - l.on) < (r.manual - r.on);
-                         });
+    std::sort(sts.begin(), sts.end(), statsSorter);
 
-        std::nth_element(sts.begin(), sts.end() - k, sts.end() - k + 1,
-                         [](const auto &l, const auto &r) {
-                             return (l.manual - l.on) < (r.manual - r.on);
-                         });
-    */
+    // std::nth_element(sts.begin(), sts.end() - k + 1, sts.end(), statsSorter);
+    // std::nth_element(sts.begin(), sts.end() - k, sts.end() - k + 1, statsSorter);
+
     size_t criticalIndex = sts.size() + 1 - k;
     int on = calcOn(sts, criticalIndex);
     int cascade = calcCascade(sts, criticalIndex, on);
     return Stats{.manual = manual, .cascade = cascade, .on = on};
 }
 
-int solve(const std::vector<Node> &parents, int k) {
+int solve(const std::vector<Node>& parents, int k) {
     size_t rootIndex = parents.size() - 1;
     auto s = solve_rec(rootIndex, parents, k);
     return s.cascade - 1;
@@ -118,7 +111,7 @@ int solveTest() {
     return solve(parents, k);
 }
 
-}  // namespace
+} // namespace
 
 int main() {
     int t = readInt();
