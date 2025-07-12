4ec5ccd586dbff206476fb3238a32e7f77c324dc
a.savanovich <asavanster@gmail.com>
2025-07-12 03:12:15 +0300

    8458 E

---
+++ ya/contest_8458/E/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm> // For std::equal
+#include <array>
+#include <iostream>
+#include <iterator> // For std::begin, std::end
+#include <string>
+
+std::string readStr() {
+    std::string x;
+    std::cin >> x;
+    return x;
+}
+
+std::array<int, 26> calcHash(const std::string& s) {
+    std::array<int, 26> array;
+    for (char c : s) {
+        ++array[c - 'a'];
+    }
+    return array;
+}
+
+int main() {
+    auto arr1 = calcHash(readStr());
+    auto arr2 = calcHash(readStr());
+    bool areEqual = arr1 == arr2;
+    std::cout << (int)areEqual << std::endl;
+    return 0;
+}
